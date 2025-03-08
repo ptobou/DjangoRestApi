d3f6a200b334c5e385e9f15ea37ed14d1de0ecc6
Patrick TOBOU <ptobou@yahoo.fr>
2025-03-08 14:09:23 +0100

    Added comment to serializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,6 +3,7 @@ from .models import Course
 
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    """This is our course serializer"""
     class Meta:
         model = Course
         fields = ('id', 'url', 'name', 'language', 'price')
